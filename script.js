2cab4b7182101bdbc5cd2440d07a6c1f6d74f971
onlis <aleksei@onl.is>
2021-11-25 13:28:30 +0500

    lesson 1 done

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = "Hello!";
+let screens = 0;
+let screenPrice = 0.5;
+let rollback = 2;
+let fullPrice = 10;
+const adaptive = "World!";
+
+alert("First ALERT!");
+console.log("and console");
