485ced06df0fc1b400f64a4eebb374c9acff06a9
onlis <aleksei@onl.is>
2021-11-26 12:24:53 +0500

    выполнены все обязательные задания урока 2

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const title = "Hello!";
-let screens = 0;
-let screenPrice = 0.5;
-let rollback = 2;
-let fullPrice = 10;
-const adaptive = "World!";
+const title = "JS Education";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 10560;
+let rollback = 15;
+let fullPrice = 99574.35;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  "Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(
+  "Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани"
+);
+console.log(screens.toLowerCase().split(","));
+console.log(fullPrice * (rollback / 100));
