5eda1a8cc83a6d2fc79d7135a9b6e015787bacac
onlis <aleksei@onl.is>
2021-12-01 13:31:08 +0500

    lesson05 simple done

--- script.js
+++ script.js
@@ -1,33 +1,80 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Какие типы экранов нужно разработать?");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
+//=============================================================================================
 
+let title, screens, adaptive, service1, servicePrice1, service2, servicePrice2;
+let screenPrice = 0;
 let servicePercentPrice = 0;
 let rollback = 15;
 let fullPrice = 0;
 let allServicePrices = 0;
 
-let getAllServicePrices = function (price1, price2) {
-  return price1 + price2;
+//=============================================================================================
+
+//-------------------------------
+const asking = function () {
+  title = getTitle(
+    prompt("Как называется ваш проект?", " КаЛьКулятор Верстки  ")
+  );
+  screens = prompt(
+    "Какие типы экранов нужно разработать?",
+    "Простые, Сложные, Адаптивные"
+  );
+  adaptive = confirm("Нужен ли адаптив на сайте?");
+  screenPrice = getScreenPrice();
+  getAllServicePrices();
 };
-let getTitle = function (title) {
+//-------------------------------
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+//-------------------------------
+let getScreenPrice = function () {
+  let value;
+  do {
+    value = prompt("Сколько будет стоить данная работа?", "15000");
+  } while (!isNumber(value));
+  return parseFloat(value);
+};
+//-------------------------------
+let getAllServicePrices = function () {
+  for (let i = 0; i < 2; i++) {
+    if (i == 0) {
+      service1 = prompt(
+        "Какой дополнительный тип услуги нужен?",
+        "Обратная связь"
+      );
+      do {
+        servicePrice1 = prompt("Сколько это будет стоить?");
+      } while (!isNumber(servicePrice1));
+      servicePrice1 = parseFloat(servicePrice1);
+    } else if (i == 1) {
+      service2 = prompt(
+        "Какой дополнительный тип услуги нужен?",
+        "Подключение телеграм бота"
+      );
+      do {
+        servicePrice2 = prompt("Сколько это будет стоить?");
+      } while (!isNumber(servicePrice2));
+      servicePrice2 = parseFloat(servicePrice2);
+    }
+  }
+  return servicePrice1 + servicePrice2;
+};
+//-------------------------------
+let getTitle = function (inpString) {
   let firstSmb;
-  title = title.trim();
-  title = title.toLowerCase();
-  firstSmb = title[0].toUpperCase();
-  title = firstSmb + title.slice(1);
-  return title;
+  inpString = inpString.trim();
+  inpString = inpString.toLowerCase();
+  firstSmb = inpString[0].toUpperCase();
+  inpString = firstSmb + inpString.slice(1);
+  return inpString;
 };
+//-------------------------------
 let getServicePercentPrices = function (fullPrice, rollback) {
   return Math.ceil(fullPrice - fullPrice * (rollback / 100));
 };
+//-------------------------------
 let screensArray = function (inputString) {
   let stringArray = [];
   inputString.split(",").forEach((element) => {
@@ -36,12 +83,11 @@ let screensArray = function (inputString) {
   });
   return stringArray;
 };
-function getFullPrice(basePrice, servicePrice) {
-  return basePrice + servicePrice;
-}
+//-------------------------------
 function showTypeOf(variable) {
   return typeof variable;
 }
+//-------------------------------
 function getRollbackMessage(fullPrice) {
   if (fullPrice >= 30000) {
     return "Даем скидку в 10%";
@@ -54,7 +100,12 @@ function getRollbackMessage(fullPrice) {
   }
 }
 
+//=============================================================================================
+
+asking();
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+//=============================================================================================
 
 console.log(showTypeOf(title));
 console.log(showTypeOf(screenPrice));
